1be736dc0f7c2a205bd7f2319832b243db01db38
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2023-11-05 16:33:11 +0100

    Generalize further.

--- precice-3/doConvergenceStudy.py
+++ precice-3/doConvergenceStudy.py
@@ -9,19 +9,6 @@ import argparse
 from enum import Enum
 
 
-default_precice_config_params = {
-    'max_used_iterations': 10,
-    'time_windows_reused': 5,
-}
-
-
-class Experiments(Enum):
-    POLYNOMIAL0 = 'p0'
-    POLYNOMIAL1 = 'p1'
-    POLYNOMIAL2 = 'p2'
-    TRIGONOMETRIC = 't'
-
-
 def render(precice_config_params):
     base_path = Path(__file__).parent.absolute()
 
@@ -38,34 +25,19 @@ def render(precice_config_params):
         file.write(precice_config_template.render(precice_config_params))
 
 
-def do_run(precice_config_params=default_precice_config_params, experiment_params={}):
-    experiment_folder = Path(__file__).parent.absolute()
-    executable = "oscillator.py"
+def do_run(precice_config_params, participants):
     render(precice_config_params)
     print(f"{datetime.datetime.now()}: Start run with parameters {precice_config_params}")
     print("Running...")
-
-    participants = [
-        {
-            "name": "Mass-Left",
-            "cmd": "Mass-Left",
-        },
-        {
-            "name": "Mass-Right",
-            "cmd": "Mass-Right",
-        },
-    ]
 
     for participant in participants:
         participant['logfile'] = f"stdout-{participant['name']}.log"
 
     for participant in participants:
-        with open(experiment_folder / participant['logfile'], "w") as outfile:
-            cmd = ["python3",
-                   experiment_folder / executable,
-                   participant["cmd"]] + [f"{opt}={value}" for opt, value in experiment_params.items()]
+        with open(participant["folder"] / participant['logfile'], "w") as outfile:
+            cmd = ["python3", participant["folder"] / participant["exec"]] + participant["params"] + [f"{keyword}={value}" for keyword, value in participant['kwargs'].items()]
             p = subprocess.Popen(cmd,
-                                 cwd=experiment_folder,
+                                 cwd=participant["folder"],
                                  stdout=outfile)
             participant["proc"] = p
 
@@ -74,15 +46,15 @@ def do_run(precice_config_params=default_precice_config_params, experiment_param
 
     for participant in participants:
         if participant["proc"].returncode != 0:
-            raise Exception(f'Experiment failed. See logs {[p["logfile"] for p in participants]}')
+            raise Exception(f"Experiment failed for participant {participant['name']}. See logs {[p['logfile'] for p in participants]}.")
 
     print("Done.")
     print("Postprocessing...")
     time_window_size = precice_config_params['time_window_size']
     summary = {"time window size": time_window_size}
     for participant in participants:
-        df = pd.read_csv(experiment_folder / f"errors-{participant['name']}.csv", comment="#")
-        summary[f"time step size {participant['name']}"] = time_window_size / experiment_params['--n-substeps']
+        df = pd.read_csv(participant["folder"] / f"errors-{participant['name']}.csv", comment="#")
+        summary[f"time step size {participant['name']}"] = time_window_size / participant['kwargs']['--n-substeps']
         summary[f"error {participant['name']}"] = df["errors"].abs().max()
     print("Done.")
 
@@ -110,24 +82,49 @@ if __name__ == "__main__":
         help="Number of refinements by factor 2 for the time step size ( >1 will result in subcycling)",
         type=int,
         default=1)
-    parser.add_argument("-e", "--experiment", help="Provide identifier for a specific experiment",
-                        choices=[e.value for e in Experiments], default=Experiments.POLYNOMIAL0.value)
     args = parser.parse_args()
 
     df = pd.DataFrame()
 
-    precice_config_params = {}
-    experiment_params = {}
+    # Define values that will be inserted into precice-config-template.xml here
+    precice_config_params = {
+        'time_window_size': '',  # will be defined later
+    }
+
+    # Define how participants will be executed here
+    participants = [
+        {
+            "name": "Mass-Left",  # identifier of this participant
+            "folder": Path(__file__).parent.absolute(),  # root folder of this participant
+            "exec": "oscillator.py",  # name of the python script, e.g. script.py
+            "params": ["Mass-Left"],  # list of positional arguments that will be used. Results in python3 script.py param1 ...
+            "kwargs": {  # dict with keyword arguments that will be used. Results in python3 script.py param1 ... k1=v1 k2=v2 ...
+                '--time-stepping': 'radauIIA',
+                '--n-substeps': '',  # will be defined later
+            },
+        },
+        {
+            "name": "Mass-Right",
+            "folder": Path(__file__).parent.absolute(),
+            "exec": "oscillator.py",
+            "params": ["Mass-Right"],
+            "kwargs": {
+                '--time-stepping': 'radauIIA',
+                '--n-substeps': '',  # will be defined later
+            },
+        },
+    ]
 
     summary_file = Path("convergence-studies") / f"{uuid.uuid4()}.csv"
 
     for dt in [args.base_time_window_size * 0.5**i for i in range(args.time_window_refinements)]:
         for n in [2**i for i in range(args.time_step_refinements)]:
+
             precice_config_params['time_window_size'] = dt
-            experiment_params['--n-substeps'] = n
-            summary = do_run(
-                precice_config_params=precice_config_params,
-                experiment_params=experiment_params)
+            for p in participants:
+                p['kwargs']['--n-substeps'] = n
+
+            summary = do_run(precice_config_params, participants)
             df = pd.concat([df, pd.DataFrame(summary, index=[0])], ignore_index=True)
 
             print(f"Write preliminary output to {summary_file}")
@@ -138,7 +135,7 @@ if __name__ == "__main__":
             print(df)
             print('-' * term_size.columns)
 
-    df = df.set_index(['time window size', 'time step size Dirichlet', 'time step size Neumann'])
+    df = df.set_index(["time window size"] + [f"time step size {p['name']}" for p in participants])
     print(f"Write final output to {summary_file}")
 
     import git
@@ -153,7 +150,7 @@ if __name__ == "__main__":
         "git commit": chash,
         "args": args,
         "precice_config_params": precice_config_params,
-        "experiment_params": experiment_params,
+        "participants": participants,
     }
 
     summary_file.unlink()
