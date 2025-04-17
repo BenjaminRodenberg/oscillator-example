43a307269d6d4e0c0c76f8abb84d7b9e7ac947a0
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-04-17 21:38:56 +0200

    Update convergence-study.sh

--- multirate/convergence-study.sh
+++ multirate/convergence-study.sh
@@ -3,4 +3,4 @@
 python3 doConvergenceStudy.py precice-config-template.xml -tss radauIIA radauIIA -is BSpline -wd 1 -sb 1 1 -o convergence-studies/bspline/multirate_radauIIA_degree1.csv
 python3 doConvergenceStudy.py precice-config-template.xml -tss radauIIA radauIIA -is BSpline -wd 2 -sb 2 2 -o convergence-studies/bspline/multirate_radauIIA_degree2.csv
 python3 doConvergenceStudy.py precice-config-template.xml -tss radauIIA radauIIA -is BSpline -wd 3 -sb 4 4 -o convergence-studies/bspline/multirate_radauIIA_degree3.csv
-python3 doConvergenceStudy.py precice-config-template.xml -tss radauIIA radauIIA -is BSpline -wd 4 -sb 8 8 -o convergence-studies/bspline/multirate_radauIIA_degree4.csv
+python3 doConvergenceStudy.py precice-config-template.xml -tss radauIIA radauIIA -is BSpline -wd 4 -w 7 -sb 8 8 -o convergence-studies/bspline/multirate_radauIIA_degree4.csv
